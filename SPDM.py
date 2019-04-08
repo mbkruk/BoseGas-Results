e472a1eeec4f2c790374f254335f210dac5d5b45
Maciej Bartłomiej Kruk <m.b.kruk@gmail.com>
2019-04-08 21:43:32 +0200

    debugging

--- SPDM.py
+++ SPDM.py
@@ -11,20 +11,25 @@ with open(sys.argv[1],"r") as f:
 	N = int(data[0])
 	nmax = int(data[1])
 	gamma = float(data[2])
+	interactionType = data[3]
+	alphaCount = int(data[4])
 
-	re = []
-	im = []
+	print("alphaCount =",alphaCount)
+	print("2*nmax+1 =",2*nmax+1)
 
-	for i in range(2*nmax+1):
-		re.append(float(data[4+i]))
-	for i in range(2*nmax+1):
-		im.append(float(data[5+2*nmax+i]))
-	alphas = np.vectorize(complex)(re,im)
-
-print(alphas)
-
-rho = np.outer(np.conj(alphas),alphas)
-
-l, v = np.linalg.eig(rho)
-print(np.abs(alphas[nmax])**2)
-print(np.max(l.real))
+	for aidx in range(alphaCount):
+		re = []
+		im = []
+		for i in range(2*nmax+1):
+			re.append(float(data[5+(2*nmax+1)*aidx+i]))
+		for i in range(2*nmax+1):
+			im.append(float(data[5+(2*nmax+1)*(aidx+1)+i]))
+		alphas = np.vectorize(complex)(re,im)
+		rho = np.outer(np.conj(alphas),alphas)
+		l, v = np.linalg.eig(rho)
+		n0 = np.abs(alphas[nmax])**2
+		rhoMax = np.max(l.real)
+		print(aidx,n0/rhoMax,n0,rhoMax,flush=True)
+		a, b = np.sum(l.real), np.sum(np.abs(alphas)**2)
+		if np.abs(a/N-1.0)>1e-3 or np.abs(b/N-1.0)>1e-3:
+			print("error",a,b)
