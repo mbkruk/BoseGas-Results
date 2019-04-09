55a4a2662ff71ade24fc20c148fa64d7259a1eb6
Maciej Bartłomiej Kruk <m.b.kruk@gmail.com>
2019-04-09 21:11:20 +0200

    proper averaging

--- SPDM.py
+++ SPDM.py
@@ -6,6 +6,8 @@ import sys
 import numpy as np
 from scipy import sparse as spsparse
 
+rhos = []
+
 with open(sys.argv[1],"r") as f:
 	data = f.read().split()
 	N = int(data[0])
@@ -13,9 +15,6 @@ with open(sys.argv[1],"r") as f:
 	gamma = float(data[2])
 	interactionType = data[3]
 	alphaCount = int(data[4])
-
-	print("alphaCount =",alphaCount)
-	print("2*nmax+1 =",2*nmax+1)
 
 	for aidx in range(alphaCount):
 		re = []
@@ -26,10 +25,9 @@ with open(sys.argv[1],"r") as f:
 			im.append(float(data[5+(2*nmax+1)*(2*aidx+1)+i]))
 		alphas = np.vectorize(complex)(re,im)
 		rho = np.outer(np.conj(alphas),alphas)
-		l, v = np.linalg.eig(rho)
-		n0 = np.abs(alphas[nmax])**2
-		rhoMax = np.max(l.real)
-		print(aidx,n0/rhoMax,n0,rhoMax,flush=True)
-		a, b = np.sum(l.real), np.sum(np.abs(alphas)**2)
-		if np.abs(a/N-1.0)>1e-3 or np.abs(b/N-1.0)>1e-3:
-			print("error",a,b)
+		rhos.append(rho)
+
+rhos = np.mean(np.array(rhos),axis=0)
+l, v = np.linalg.eig(rhos)
+
+print(np.sort(l.real))
