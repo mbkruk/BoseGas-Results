5bdce2dc8a603dd0f820e3b701a2249d91fba57a
mjlk1 <maclebek@gmail.com>
2019-05-26 18:10:42 +0200

    correlation length script

---
+++ CorrelationLength.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import optimize as spopt
+
+if len(sys.argv)!=2:
+	exit(1)
+
+def psi(x,k,alphas):
+	return np.sum(alphas*np.exp(np.outer(2j*np.pi*x,k)),axis=1)
+
+
+with open(sys.argv[1],"r") as f:
+	data = f.read().split()
+	N = int(data[0])
+	nmax = int(data[1])
+	gamma = float(data[2])
+	interactionType = data[3]
+	alphaCount = int(data[4])
+
+	resol =100
+	x = np.linspace(-0.5,0.5,resol)
+
+	k = []
+	for i in range(-nmax,nmax+1):
+		k.append(i)
+	k = np.array(k)
+
+	values=[]
+	for i in range(resol):
+		values.append(0)
+
+	for aidx in range(alphaCount):
+		re = []
+		im = []
+		for i in range(2*nmax+1):
+			re.append(float(data[5+(2*nmax+1)*2*aidx+i]))
+		for i in range(2*nmax+1):
+			im.append(float(data[5+(2*nmax+1)*(2*aidx+1)+i]))
+		alphas = np.vectorize(complex)(re,im)
+
+		y = np.abs(psi(x,k,alphas))**2
+
+		for i in range(resol):
+			values[i]+=np.abs(psi(0,k,alphas)*psi(x[i],k,alphas))
+
+	values=np.array(values)
+	values=values/alphaCount
+for i in range(resol):
+	print(x[i],values[i])
+
+plt.plot(x,values)
+plt.show()
