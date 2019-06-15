f4955a3fa4d0d5ad5f8a4288bd239c5b19e4f268
mjlk1 <maclebek@gmail.com>
2019-06-15 23:32:22 +0200

    correlation changes

--- CorrelationLength.py
+++ CorrelationLength.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import BoseGas as bg
 
-def findNearest(array, value):
+def FindNearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
@@ -24,6 +24,7 @@ for i in range(-data.nmax,data.nmax+1):
 k = np.array(k)
 
 values = np.zeros(resol,dtype=complex)
+densityValues = np.zeros(resol,dtype=float)
 
 status = bg.Status()
 
@@ -31,18 +32,20 @@ for aidx in range(data.alphaCount):
 	status.update(aidx,data.alphaCount)
 	alphas = data.loadAlphas()
 	psix = np.array(bg.psi(x,k,alphas))
+	densityValues += np.abs(psix)**2
 	values += np.conj(bg.psi(0.0,k,alphas))*psix
 
 values = np.abs(values) / data.alphaCount
-
+densityValues = np.sqrt(densityValues / data.alphaCount)
+values = np.divide(values,densityValues[list(x).index(0)]*densityValues)
 plt.plot(x,values)
 plt.show()
 
 tab1 = values[: list(x).index(0)+1]
 tab2 = values[list(x).index(0):]
-hmax1 = (max(tab1) - min(tab1)) / 2.0 + min(tab1)
-hmax2 = (max(tab2) - min(tab2)) / 2.0 + min(tab2)
+hmax1 = (max(tab1) - min(tab1))/2.0 + min(tab1)
+hmax2 = (max(tab2) - min(tab2))/2.0 + min(tab2)
 
-print(values[list(x).index(0)])
-print(x[findNearest(tab1,hmax1)])
-print(x[list(x).index(0)+findNearest(tab2,hmax2)])
+print(x[FindNearest(tab1,hmax1)])
+print(x[list(x).index(0)+FindNearest(tab2,hmax2)])
+print((np.abs(x[list(x).index(0)+FindNearest(tab2,hmax2)]-x[FindNearest(tab1,hmax1)]))/2.0)
