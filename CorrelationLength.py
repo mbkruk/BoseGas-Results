031b45d300b82675e6fa88b7c0137150c33d2cef
mjlk1 <maclebek@gmail.com>
2019-06-15 20:24:07 +0200

    changes in CorrelationLength

--- CorrelationLength.py
+++ CorrelationLength.py
@@ -3,15 +3,19 @@
 import sys
 import numpy as np
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import BoseGas as bg
+
+def findNearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
 
 if len(sys.argv)!=2:
 	exit(1)
 
 data = bg.MCData(sys.argv[1])
 
-resol = 32
+resol = 101
 x = np.linspace(-0.5,0.5,resol)
 
 k = []
@@ -19,24 +23,26 @@ for i in range(-data.nmax,data.nmax+1):
 	k.append(i)
 k = np.array(k)
 
-values = []
+values = np.zeros(resol,dtype=complex)
 
 status = bg.Status()
 
 for aidx in range(data.alphaCount):
 	status.update(aidx,data.alphaCount)
 	alphas = data.loadAlphas()
-	values.append(np.outer(np.conj(bg.psi(x,k,alphas)),bg.psi(x,k,alphas)))
+	psix = np.array(bg.psi(x,k,alphas))
+	values += np.conj(bg.psi(0.0,k,alphas))*psix
 
-values = np.array(values)
+values = np.abs(values) / data.alphaCount
 
-X, Y = np.meshgrid(x,x)
-
-m = np.abs(np.mean(values,axis=0))
-#s = np.abs(np.std(values,axis=0))
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_surface(X,Y,m)
-#ax.plot_surface(X,Y,s)
+plt.plot(x,values)
 plt.show()
+
+tab1 = values[: list(x).index(0)+1]
+tab2 = values[list(x).index(0):]
+hmax1 = (max(tab1) - min(tab1)) / 2.0 + min(tab1)
+hmax2 = (max(tab2) - min(tab2)) / 2.0 + min(tab2)
+
+print(values[list(x).index(0)])
+print(x[findNearest(tab1,hmax1)])
+print(x[list(x).index(0)+findNearest(tab2,hmax2)])
