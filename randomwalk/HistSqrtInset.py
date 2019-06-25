a799b07335bfa1dbe39f42e89dfe810b9ff09f01
Maciej Bartłomiej Kruk <m.b.kruk@gmail.com>
2019-06-25 22:02:41 +0200

    inset randomwalk plots, calculate chi^2 etc

---
+++ randomwalk/HistSqrtInset.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+import BoseGas as bg
+from matplotlib import pyplot as plt
+from scipy import optimize as spopt
+
+fname = sys.argv[1]
+
+ldx = np.load(fname)
+
+fig, ax1 = plt.subplots()
+left, bottom, width, height = [0.50, 0.2, 0.35, 0.35]
+ax2 = fig.add_axes([left, bottom, width, height])
+
+# hist
+
+ax2.set_xlabel("$\Delta x$")
+ax2.set_ylabel("$p(\Delta x)$")
+n, bins, labels = plt.hist(ldx,bins=51,density=True)
+
+maxi = np.argmax(n)
+delta0 = n[maxi]
+mids = []
+vals = []
+maxh = 0.0
+for i in range(len(n)):
+	if i==maxi:
+		continue
+	mids.append((bins[i]+bins[i+1])/2)
+	vals.append(n[i])
+	maxh = max(n[i],maxh)
+
+def gauss(x,mu,sigma,N):
+	return N*np.exp(-0.5*((x-mu)/sigma)**2)/np.sqrt(np.pi)/sigma
+
+def gaussp(x,mu,sigma,N):
+	return gauss(x-1,mu,sigma,N)+gauss(x,mu,sigma,N)+gauss(x+1,mu,sigma,N)
+
+xmax = (bins[maxi+1]+bins[maxi])/2
+
+hp, hpcov = spopt.curve_fit(gaussp,mids,vals,p0=(xmax,0.002,0.5))
+hu = np.sqrt(np.diag(hpcov))
+
+print(hp)
+print(hu,flush=True)
+
+# sqrt
+
+mds = 10
+mdn = 100
+
+dist = [list() for _ in range(mdn)]
+y = []
+for i in range(mdn,len(ldx),2*mdn):
+	x = 0.0
+	for j in range(mdn):
+		x += ldx[i+j-mdn]
+		dist[j].append(np.abs(x))
+		if i==mdn:
+			y.append(x)
+dist = np.array(dist)
+t = np.arange(mdn)+1
+
+dist = dist[mds:]
+t = t[mds:]
+
+mdm = np.mean(dist,axis=1)
+mdu = np.std(dist,axis=1,ddof=1)/np.sqrt(dist.shape[1])
+
+def f(x,a,b):
+	return b+np.sqrt(x/a)
+
+mdp, mdpcov = spopt.curve_fit(f,t,mdm)
+
+mdchi2red = np.sum(((f(t,*mdp)-mdm)/mdu)**2)/(len(t)-len(mdp))
+
+print("MD chi^2_r =",mdchi2red,flush=True)
+
+ax2.set_ylim((0,(maxh+min(delta0,2*maxh))/2*1.05))
+x = np.linspace(np.min(ldx),np.max(ldx),256)
+y = gauss(x,*hp)
+ax2.plot(x,y)
+
+ax1.set_xlabel("MC steps")
+ax1.set_ylabel("$<|x|>$")
+
+ax1.errorbar(t,mdm,yerr=mdu)
+x = np.linspace(0,mdn,256)
+ax1.plot(x,f(x,*mdp))
+
+output = "inset-"+(fname.replace(".npy",".pdf"))
+plt.savefig(output)
+print(output,flush=True)
+
+plt.show()
--- randomwalk/MeanDistance.py
+++ randomwalk/MeanDistance.py
@@ -6,6 +6,7 @@ import BoseGas as bg
 from matplotlib import pyplot as plt
 from scipy import optimize as spopt
 
+s = 5
 n = 100
 
 fname = sys.argv[1]
@@ -24,22 +25,27 @@ for i in range(n,len(ldx),2*n):
 		if i==n:
 			y.append(x)
 dist = np.array(dist)
-print(dist.shape,flush=True)
 t = np.arange(n)+1
+
+dist = dist[s:]
+t = t[s:]
+
 m = np.mean(dist,axis=1)
-u = np.std(dist,axis=1,ddof=1)
-print(m.shape,flush=True)
+u = np.std(dist,axis=1,ddof=1)/np.sqrt(dist.shape[1])
 
-def f(x,a):
-	return np.sqrt(x/a)
+def f(x,a,b):
+	return b+np.sqrt(x/a)
 
-popt, pcov = spopt.curve_fit(f,t,m)
+popt, pcov = spopt.curve_fit(f,t,m,sigma=u)
+
+chi2 = np.sum(((f(t,*popt)-m)/u)**2)/(len(t)-len(popt))
+
+print(chi2,flush=True)
 
 plt.xlabel("MC steps")
 plt.ylabel("$<|x|>$")
 
 plt.errorbar(t,m,yerr=u)
-#plt.plot(t,y)
 x = np.linspace(0,n,256)
 plt.plot(x,f(x,*popt))
 
