cb2c07771826709a14cef81cd3ca1a3e7157805d
Maciej Bartłomiej Kruk <m.b.kruk@gmail.com>
2019-08-13 21:21:03 +0200

    new new cutoff plot script

--- correlation/PlotRatio.py
+++ correlation/PlotRatio.py
@@ -5,13 +5,35 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import optimize as spopt
 
-x, y = np.loadtxt(sys.argv[1]).T
+nmax, ratio = np.loadtxt(sys.argv[1]).T
 
-def f(x,a):
-	return np.sqrt(x/a)
+what = sys.argv[2]
 
-popt, pcov = spopt.curve_fit(f,x,y)
+# beta = 0.58/nmax/nmax
+# nmax = sqrt(0.58/beta)
+
+oldbeta = 0.58/nmax/nmax
+newbeta = ratio*oldbeta
+effconst = ratio*0.58#/nmax/nmax*nmax*nmax
+
+def f(x,a,b):
+	return np.power(x/a,b)
+
+if what=="newbeta/nmax":
+	x, y, popt  = newbeta, nmax, (1/18,-0.5)
+elif what=="effconst":
+	x, y, popt  = nmax, effconst, (18,0.5)
+
+popt, pcov = spopt.curve_fit(f,x,y,p0=popt)
+
+print(*popt,flush=True)
+
+if what=="effconst":
+	plt.axhline(y=0.58,color="black")
 
 plt.scatter(x,y)
+if what=="newbeta/nmax":
+	plt.scatter(oldbeta,nmax)
+
 plt.scatter(x,f(x,*popt),marker='x')
 plt.show()
