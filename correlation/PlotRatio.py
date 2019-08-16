e3ad4175ff849d2312685ca447d7fbd570328d1b
mjlk1 <maclebek@gmail.com>
2019-08-16 19:07:56 +0200

    old plots deleted

--- correlation/PlotRatio.py
+++ correlation/PlotRatio.py
@@ -29,11 +29,13 @@ popt, pcov = spopt.curve_fit(f,x,y,p0=popt)
 print(*popt,flush=True)
 
 if what=="effconst":
-	plt.axhline(y=0.58,color="black")
+	plt.axhline(y=0.58,color="black", linestyle='dashed')
+	plt.xlabel('$n_{max}$')
+	plt.ylabel('$\\beta n_{max}^2$')
 
 plt.scatter(x,y)
 if what=="newbeta/nmax":
 	plt.scatter(oldbeta,nmax)
 
-plt.scatter(x,f(x,*popt),marker='x')
+#plt.scatter(x,f(x,*popt),marker='x')
 plt.show()
