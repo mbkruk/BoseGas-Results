86154170ea29d2da2525eadcfe7821751cefd9cb
Maciej Bartłomiej Kruk <m.b.kruk@gmail.com>
2019-09-04 19:28:53 +0200

    localfluc: new data

--- localfluc/MakeLocalFluc.sh
+++ localfluc/MakeLocalFluc.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
 
-HELP="MakeLocalFluc.sh <0/1/2/3> <output dir>"
+HELP="MakeLocalFluc.sh <0/1/2/3> <mc/max> <output dir>"
+AVERAGE_GAMMA="-0.011871523554730593"
+E_CONTACT=8
+E_DIPOLE=3
+MC_COUNT=32
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 3 ]
 then
 	echo $HELP
 	exit 1
 fi
 
-DATA=$2
+LF_METHOD=$2
+DATA=$3
+
 case "$1" in
 	"0")
-	NAME="n1-e7-contact"
-	CMD="bgmc -N 100 -n 1 -e 7 -a"
+	NAME="n1-e${E_CONTACT}-contact"
+	CMD="bgmc -N 100 -n 1 -e $E_CONTACT -g $AVERAGE_GAMMA -a"
 	;;
 	"1")
-	NAME="n3-e7-contact"
-	CMD="bgmc -N 100 -n 3 -e 7 -a"
+	NAME="n3-e${E_CONTACT}-contact"
+	CMD="bgmc -N 100 -n 3 -e $E_CONTACT -g $AVERAGE_GAMMA -a"
 	;;
 	"2")
-	NAME="n1-e7-dipole-0.060"
-	CMD="bgmc -N 100 -n 1 -e 7 -g -0.032035302086685036 -I ../intcoeff/dipol-0.060.in -a"
+	NAME="n1-e${E_DIPOLE}-dipole-0.060"
+	CMD="bgmc -N 100 -n 1 -e $E_DIPOLE -g $AVERAGE_GAMMA -I ../intcoeff/dipol-0.060.in -a"
 	;;
 	"3")
-	NAME="n3-e7-dipole-0.060"
-	CMD="bgmc -N 100 -n 3 -e 7 -g -0.032035302086685036 -I ../intcoeff/dipol-0.060.in -a"
+	NAME="n3-e${E_DIPOLE}-dipole-0.060"
+	CMD="bgmc -N 100 -n 3 -e $E_DIPOLE -g $AVERAGE_GAMMA -I ../intcoeff/dipol-0.060.in -a"
 	;;
 	*)
 	echo $HELP
 	exit 1
 esac
 
-for N in {1..32}
+for N in $(seq -f "%0${#MC_COUNT}g" 1 $MC_COUNT)
 do
+	echo "### $N/$MC_COUNT ###"
 	$CMD -o $DATA$NAME-$N.txt
 done
 
 ./combine $DATA$NAME-*.txt > $DATA$NAME.in.txt
 
-./localfluc mc < $DATA$NAME.in.txt > $DATA$NAME.out.txt
+./localfluc $LF_METHOD < $DATA$NAME.in.txt > $DATA$NAME.$LF_METHOD.out.txt
